2efc305920a18e0e720ca17c60d16ade3591d825
chanhpar <chanhpar@student.42seoul.kr>
2023-04-03 17:18:16 +0900

    fix ex12

--- ex12/smtp.hs
+++ ex12/smtp.hs
@@ -1,52 +1,55 @@
-{-# OPTIONS -Wall -Wextra #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wall -Wextra #-}
 
-import Data.Text.Internal qualified
-import Data.Text.Lazy qualified
-import Network.HaskellNet.Auth
+import Data.Maybe
+import Data.Text qualified
+import Data.Text.IO qualified
+import Data.Text.Lazy.IO qualified
+import Network.HaskellNet.Auth qualified
 import Network.HaskellNet.SMTP
 import Network.HaskellNet.SMTP.SSL
 import Network.Mail.Mime
+import System.Console.Haskeline
 
 -- | Your settings
 server :: String
 server = "smtp.naver.com"
 
--- port         = toEnum 587
-
-username :: Network.HaskellNet.Auth.UserName
-username = "foo"
-
-password :: Network.HaskellNet.Auth.Password
-password = "bar"
-
 authType :: Network.HaskellNet.Auth.AuthType
 authType = Network.HaskellNet.Auth.PLAIN
 
-from :: Network.Mail.Mime.Address
-from = "foo@bar.com"
+mysettings :: System.Console.Haskeline.Settings IO
+mysettings = defaultSettings{historyFile = Nothing}
 
-to :: Network.Mail.Mime.Address
-to = "hello@world.com"
+attachments :: [(Data.Text.Text, FilePath)]
+attachments = [("text/plain", "./smtp.hs")]
 
-subject :: Data.Text.Internal.Text
-subject = "Network.HaskellNet.SMTP Test :)"
+getAddr :: IO Network.Mail.Mime.Address
+getAddr = Network.Mail.Mime.Address Nothing <$> Data.Text.IO.getLine
 
-plainBody :: Data.Text.Lazy.Text
-plainBody = "Hello world!"
-
-htmlBody :: Data.Text.Lazy.Text
-htmlBody = "<html><head></head><body><h1>Hello <i>world!</i></h1></body></html>"
-
-attachments :: [(Data.Text.Internal.Text, FilePath)]
-attachments = [("application/zip", "~/Downloads/attachments.zip"), ("text/plain", "./smtp.hs")]
-
--- | With authentication
 main :: IO ()
-main = doSMTPSTARTTLS server $ \conn -> do
-    authSuccess <- authenticate authType username password conn
-    if authSuccess
-        then do
-            mail <- simpleMail to from subject plainBody htmlBody attachments
-            sendMail mail conn
-        else putStrLn "Authentication failed."
+main = do
+    putStr "naver login id: "
+    username <- getLine
+    password <- runInputT mysettings (getPassword (Just '*') "login password: ")
+
+    let fromAddr = username <> "@naver.com"
+    putStrLn ("sender address: " <> fromAddr)
+
+    let from = Network.Mail.Mime.Address Nothing . Data.Text.pack $ fromAddr
+
+    putStr "reciever address: "
+    to <- getAddr
+
+    putStrLn "title:"
+    subject <- Data.Text.IO.getLine
+    putStrLn "content:"
+    htmlBody <- Data.Text.Lazy.IO.getLine
+
+    doSMTPSTARTTLS server $ \conn -> do
+        authSuccess <- authenticate authType username (fromJust password) conn
+        if authSuccess
+            then do
+                mail <- simpleMail to from subject "" htmlBody attachments
+                sendMail mail conn
+            else putStrLn "Authentication failed."
